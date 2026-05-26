bf60ffca2643374f13cb859644dc0f4e6ed7a3bb
picpal <picpal@kakao.com>
2026-05-26 22:34:40 +0900

    fix: add scanBasePackages=com.picpay to all services for GlobalExceptionHandler scan

--- services/gateway/src/main/java/com/picpay/gateway/GatewayApplication.java
+++ services/gateway/src/main/java/com/picpay/gateway/GatewayApplication.java
@@ -3,7 +3,7 @@ package com.picpay.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.picpay")
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
--- services/token/src/main/java/com/picpay/token/TokenApplication.java
+++ services/token/src/main/java/com/picpay/token/TokenApplication.java
@@ -3,7 +3,7 @@ package com.picpay.token;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.picpay")
 public class TokenApplication {
     public static void main(String[] args) {
         SpringApplication.run(TokenApplication.class, args);
